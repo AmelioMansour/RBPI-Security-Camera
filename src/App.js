8a78ae037376586a28cea0769dc138f9194a376a
AmelioMansour <ameliomansour@oakland.edu>
2023-11-21 18:51:34 -0500

    test 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+          <h1>Security Camera</h1>
+          <p>CSI4160</p>
+            <video src="https://storage.googleapis.com/security_videos_csi4160/Security%2014-11-2023-22-56-30.mp4" controls></video>
+        </div>
+    );
 }
 
 export default App;
